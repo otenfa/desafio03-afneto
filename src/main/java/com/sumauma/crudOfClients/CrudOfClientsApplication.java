2fe7725589fd71df85e5fb4bec3432faebb62033
Aristóteles Ferrão <afneto1960@gmail.com>
2025-01-02 11:54:46 -0300

    first commit

---
+++ src/main/java/com/sumauma/crudOfClients/CrudOfClientsApplication.java
@@ -0,0 +1,13 @@
+package com.sumauma.crudOfClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOfClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOfClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumauma/crudOfClients/CrudOfClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumauma.crudOfClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOfClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
