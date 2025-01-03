45689b5ac421d5fbdece3e1b7e3b36ad02e3a955
Aristóteles Ferrão <afneto1960@gmail.com>
2025-01-03 15:50:59 -0300

    criação da entidade Client e carga de dados

--- src/main/java/com/sumauma/crudOfClients/CrudOfClientsApplication.java
+++ src/main/java/com/sumauma/crudOfClients/CrudOfClientsApplication.java
@@ -1,13 +1,21 @@
 package com.sumauma.crudOfClients;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudOfClientsApplication {
+public class CrudOfClientsApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudOfClientsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Rotinas de iniciais finalizadas!");
+		
 	}
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ella Jane Fitzgerald', '35271624860', 5000.00, '1917-04-25', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eleanora Fagan', '26983710534', 15000.00, '1915-07-04', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sarah Lois Vaughan', '52316487040', 15000.00, '1924-03-27', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eunice Kathleen Waymon', '93416720300', 5000.00, '1933-02-21', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ruth Lee Jones', '70839425172', 5000.00, '1924-08-29', 8);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bessie Smith', '12695830789', 8000.00, '1894-04-15', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Norma Deloris Egstrom', '38405261908', 20000.00, '1920-05-26', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lilli Mae Jones', '81724597345', 7000.00, '1929-05-16', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carmen Mercedes McRae', '47039156205', 3500.00, '1920-08-04', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Querida Margarethe Blossom', '54123948061', 5000.00, '1924-04-28', 0);
+
